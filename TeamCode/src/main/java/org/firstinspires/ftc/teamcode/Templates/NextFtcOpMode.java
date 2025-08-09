80d541b98e0d4bf3f232d3d0d2a7de76ef11cafc
Robo - Phantoms <robophantoms@gmail.com>
2025-08-09 12:52:53 -0500

    Added templates

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Templates/NextFtcOpMode.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.Templates;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.rowanmcalpin.nextftc.ftc.NextFTCOpMode;
+
+@TeleOp(name = "")
+@Autonomous(name = "")
+public class NextFtcOpMode extends NextFTCOpMode {
+
+    //put subsystems here
+    public NextFtcOpMode(){
+        super();
+    }
+    @Override
+    public void onInit(){}
+    @Override
+    public void onStartButtonPressed(){}
+    @Override
+    public void onUpdate(){}
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Templates/subsystem.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.Templates;
+
+import com.rowanmcalpin.nextftc.core.Subsystem;
+
+import org.firstinspires.ftc.teamcode.subsystems.Slides;
+
+public class subsystem extends Subsystem {
+    //Boilerplate
+    public static final subsystem INSTANCE = new subsystem();
+    private subsystem() {}
+
+    @Override
+    public void periodic(){}
+    @Override
+    public void initialize(){}
+}
