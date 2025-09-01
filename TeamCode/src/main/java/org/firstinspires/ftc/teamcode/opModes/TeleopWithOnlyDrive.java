88d5b238d0c8470028804f57d68f2f83d6060326
Robo - Phantoms <robophantoms@gmail.com>
2025-09-01 15:51:03 -0500

    hehe

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/TeleopWithOnlyDrive.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.opModes;
+
+import androidx.annotation.RequiresPermission;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.commands.utility.LambdaCommand;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.controllable.MotorGroup;
+import dev.nextftc.hardware.driving.DifferentialTankDriverControlled;
+import dev.nextftc.hardware.driving.MecanumDriverControlled;
+import dev.nextftc.hardware.impl.MotorEx;
+import org.firstinspires.ftc.teamcode.util.*;
+
+
+@TeleOp(name="TeleopWithOnlyDrive")
+public class TeleopWithOnlyDrive extends NextFTCOpMode {
+
+    public TeleopWithOnlyDrive(){}
+
+    public MotorEx leftFront;
+    public MotorEx rightFront;
+    public MotorEx leftBack;
+    public MotorEx rightBack;
+    public MotorGroup leftMotors;
+    public MotorGroup rightMotors;
+
+    @Override
+    public void onInit() {
+        leftFront = new MotorEx("leftFront").reversed();
+        rightFront = new MotorEx("rightFront");
+        leftBack = new MotorEx("leftBack").reversed();
+        rightBack = new MotorEx("rightBack");
+        leftMotors = new MotorGroup(leftFront, leftBack);
+        rightMotors = new MotorGroup(rightFront, rightBack);
+    }
+
+    @Override
+    public void onStartButtonPressed() {
+        Command driverControlled = new DifferentialTankDriverControlled(leftMotors, rightMotors, Gamepads.gamepad1().leftStickY(), Gamepads.gamepad1().rightStickY());
+        driverControlled.schedule();
+
+        Gamepads.gamepad2().leftBumper()
+                .whenTrue(() -> ExtraCommands.strafeLeft(leftFront, rightFront, leftBack, rightBack))
+                .whenBecomesFalse(() -> ExtraCommands.stop(leftFront, rightFront, leftBack, rightBack));
+        Gamepads.gamepad2().rightBumper()
+                .whenTrue(() -> ExtraCommands.strafeRight(leftFront, rightFront, leftBack, rightBack))
+                .whenBecomesFalse(() -> ExtraCommands.stop(leftFront, rightFront, leftBack, rightBack));
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ExtraCommands.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.LambdaCommand;
+import dev.nextftc.hardware.controllable.Controllable;
+
+public class ExtraCommands {
+    public static double STRAFE_POWER = 0.75;
+
+    public static void strafeLeft(Controllable lfMotor, Controllable rfMotor, Controllable lbMotor, Controllable rbMotor) {
+        rfMotor.setPower(STRAFE_POWER);
+        lfMotor.setPower(-STRAFE_POWER);
+        lbMotor.setPower(STRAFE_POWER);
+        rbMotor.setPower(-STRAFE_POWER);
+    }
+
+    public static void strafeRight(Controllable lfMotor, Controllable rfMotor, Controllable lbMotor, Controllable rbMotor) {
+        rfMotor.setPower(-STRAFE_POWER);
+        lfMotor.setPower(STRAFE_POWER);
+        lbMotor.setPower(-STRAFE_POWER);
+        rbMotor.setPower(STRAFE_POWER);
+    }
+
+    public static void stop(Controllable lf, Controllable rf, Controllable lb, Controllable rb) {
+        lf.setPower(0);
+        rf.setPower(0);
+        lb.setPower(0);
+        rb.setPower(0);
+    }
+}
